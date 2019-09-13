5f402e2387eea7dda21fdfcf044a464531b9a787
Sidnei <sialkas@gmail.com>
2019-09-13 13:52:30 -0300

    Update Layout

--- public/message.js
+++ public/message.js
@@ -1,5 +1,6 @@
 //const socket = io('http://localhost:3000');
-const socket = io('http://192.168.0.107:3000');
+const socket = io('http://192.168.15.30:3000'); 
+//const socket = io('http://192.168.0.107:3000');
 
 function beep(){
 
@@ -82,6 +83,5 @@ $('#chat').submit(function(event) {
     }
 
   }
-
 
 });
